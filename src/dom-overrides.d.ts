1a3a673ccda6abd9db53b9097d393f262d10bf51
Hotaru(Naokibot) <sagakatsuyuki.555@gmail.com>
2026-08-15 05:08:37 +0900

    Restore general querySelector overloads

--- src/dom-overrides.d.ts
+++ src/dom-overrides.d.ts
@@ -3,6 +3,10 @@ export {}
 declare global {
   interface Document {
     querySelector<E extends Element = Element>(selectors: '#app'): E
+    querySelector<K extends keyof HTMLElementTagNameMap>(selectors: K): HTMLElementTagNameMap[K] | null
+    querySelector<K extends keyof SVGElementTagNameMap>(selectors: K): SVGElementTagNameMap[K] | null
+    querySelector<K extends keyof MathMLElementTagNameMap>(selectors: K): MathMLElementTagNameMap[K] | null
+    querySelector<E extends Element = Element>(selectors: string): E | null
   }
 
   interface HTMLInputElement {
